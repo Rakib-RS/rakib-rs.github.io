6142bc89d9432f1105063dbc3052fc7d86aff3c3
rakib-rs <rakib.rscse@gmail.com>
2021-08-28 00:01:34 +0600

    Educational details added

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,17 +1,40 @@
-import React from 'react'
-import './Education.css'
-const Education = props => {
-    return (
-        <div className="education">
-            <h4>Education</h4>
-            <div className="educontainer">
-                ok
-
+import React from "react";
+import "./Education.css";
+const Education = (props) => {
+  return (
+    <div className="education">
+      <h4>Education</h4>
+      <div className="educontainer">
+        <div className="content">
+          <div className="school">
+            <div className="icon">
+              <h3>S</h3>
             </div>
+            <h6>SSC-2014</h6>
+            <h5>Gouripur R.K. GOVT. High School</h5>
+          </div>
         </div>
-    )
-}
-
-
+        <div className="content smargin">
+          <div className="school">
+            <div className="icon">
+              <h3>C</h3>
+            </div>
+            <h6>HSC-2016</h6>
+            <h5>Gouripur R.K. GOVT. High School</h5>
+          </div>
+        </div>
+        <div className="content smargin">
+          <div className="school">
+            <div className="icon">
+              <h3>U</h3>
+            </div>
+            <h6>BSc(CSE) 2017-current</h6>
+            <h5>Gouripur R.K. GOVT. High School</h5>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Education;
